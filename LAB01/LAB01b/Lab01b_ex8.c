15297e1d55d9c2b0728895846255b4c067f59435
Cloud User <ec2-user@ip-172-31-43-158.ec2.internal>
2023-08-30 16:41:56 +0000

    Criado e compliado arquivo em c do exercicio 8.

---
+++ LAB01/LAB01b/Lab01b_ex8.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void transposta(int linha, int coluna, int matriz[linha][coluna]){
+  int transposta[coluna][linha];
+  
+  for(int i=0;i<linha;i++){ 
+    for(int j=0; j<coluna; j++)
+    {
+      transposta[j][i] = matriz[i][j];
+    }
+  }
+
+  printf("\n\nMatriz Transposta:\n");
+  for (int i=0; i<linha; i++ ){ 
+    for (int j=0; j<coluna; j++ )
+    {
+      printf ("%d ", transposta[ i ][ j ]);
+    }
+    printf("\n");
+  }
+}
+
+
+int main(void) {
+  //exercicio 8
+ int linha, coluna;
+  
+  printf("\nDigite o numero de linha(s) da matriz:");
+  scanf("%d", &linha);
+
+  printf("\nDigite o numero de coluna(s) da matriz:");
+  scanf("%d", &coluna);
+
+  int matriz[linha][coluna];
+
+  for(int i=0;i<linha;i++){ 
+    for(int j=0; j<coluna; j++)
+    {
+      printf ("\n Digite o Elemento[%d][%d] = ", i, j);
+      scanf ("%d", &matriz[i][j]);
+    }
+  }
+
+  transposta(linha, coluna, matriz);
+  return 0;
+}
